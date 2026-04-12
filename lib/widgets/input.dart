1f76ffdd5e333ce8b0bd59c4acd9ea76798aada3
chen08209 <chen08209@gmail.com>
2026-04-12 08:35:59 +0800

    cache

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -82,6 +82,8 @@ class CommonCheckBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Checkbox(
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      visualDensity: VisualDensity.compact,
       shape: isCircle ? const CircleBorder() : null,
       value: value,
       onChanged: onChanged,
